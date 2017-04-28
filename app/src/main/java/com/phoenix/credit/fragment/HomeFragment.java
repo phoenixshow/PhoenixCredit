81fc810f23069b0fc4d2d3cae7f4034ae0e04770
phoenixshow <965550123@qq.com>
2017-04-28 23:12:31 +0800

    添加通用Title，并用include标签包含复用

--- app/src/main/java/com/phoenix/credit/fragment/HomeFragment.java
+++ app/src/main/java/com/phoenix/credit/fragment/HomeFragment.java
@@ -6,18 +6,47 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.phoenix.credit.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Created by flashing on 2017/4/28.
  */
 
 public class HomeFragment extends Fragment {
+    @BindView(R.id.iv_title_back)
+    ImageView ivTitleBack;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.iv_title_setting)
+    ImageView ivTitleSetting;
+    Unbinder unbinder;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = View.inflate(getActivity(), R.layout.fragment_home, null);
+        unbinder = ButterKnife.bind(this, view);
+        //初始化Title
+        initTitle();
         return view;
+    }
+
+    private void initTitle() {
+        ivTitleBack.setVisibility(View.GONE);
+        tvTitle.setText(R.string.home);
+        ivTitleSetting.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
